6daaabba777591e08cdd11ea84d601a18021657d
9lll2lll <109672465+9lll2lll@users.noreply.github.com>
2023-05-15 05:08:34 +0300

    config.py

--- config.py
+++ config.py
@@ -25,8 +25,8 @@ class Config(object):
     PROCESS_MAX_TIMEOUT = 3600
     # your telegram id
     OWNER_ID = int(os.environ.get("OWNER_ID", "887108671"))
-    SESSION_NAME = "1ApWapzMBu3ovTJgp-xdmo-ujOni9vM7vShRpcLW609aJCmM0_y6NDbko5PEJm4vzVVavZX74fkKxbMnMyQMxEwX5W-hAjqjpW2u-miMryZSaYNY36Iu9nfd2yPJyYLet5yUhksCtMRL-0dvYjgpKX5xuhkXn-fQBY5QGvXR97IIh64mzkxXr6KlVq0skdTyoTZwiVafSnDXucoJLIFlhSiP3QrlaWS-9J2DFhjd35M_fj14xGeDWk60Lz8jLaevOQPS6hrQgCx8nJEGfi-XeWlRaJLWN52pKomu1Njm0wqaXgjgdFBFoQ38x24uOP5YNSqJ0UONTb9aLJ4eYVo57awmOfVY6LBk="
+    SESSION_NAME = "UPLOADER-X-BOT"
     # database uri (mongodb)
     DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://db-mongodb-ams3-25994-51438eff.mongo.ondigitalocean.com")
     MAX_RESULTS = "50"
-    PREMIUM_USER = os.environ.get("io9868")
+    PREMIUM_USER = os.environ.get("PREMIUM_USER")
